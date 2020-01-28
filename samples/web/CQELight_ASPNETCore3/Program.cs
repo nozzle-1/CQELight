656dadd25d8d1f1e02599d7f714e3b2de6fc23de
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2020-01-28 17:18:27 +0100

    create an overload for configuration in configure method

--- samples/web/CQELight_ASPNETCore3/Program.cs
+++ samples/web/CQELight_ASPNETCore3/Program.cs
@@ -11,15 +11,18 @@ namespace CQELight_ASPNETCore3
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureCQELight(b =>
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureCQELight((b, config) =>
                 {
+                    var conf = config["conf"]; // Access to configuration right here for connection strings or so
                     b.UseAutofacAsIoC();
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        }
     }
 }
